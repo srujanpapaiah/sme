9a7b72ac8ee4f825478e3d6db1b16efa60e8a2cf
Srujan Papaiahgari <srujanpapaiahgari@gmail.com>
2023-09-13 19:04:19 +0530

    added protected routes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { toast, Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function Profile() {
+  const router = useRouter();
+  const logoutHandle = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster />
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <button
+        onClick={logoutHandle}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
