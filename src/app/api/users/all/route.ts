01467b524aaaed1c2fc5c543e9c5eb1a5a3e28a3
srujan-ineuron <srujan@ineuron.ai>
2023-10-23 11:20:53 +0530

    added all users endpoint

--- src/app/api/users/all/route.ts
+++ src/app/api/users/all/route.ts
@@ -3,3 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 import User from "@/models/userModel";
 import connect from "@/dbConfig/dbConfig";
+
+export async function GET(request: NextRequest) {
+  try {
+    connect();
+    const response = await User.find();
+
+    const nextResponse = NextResponse.json({ data: response });
+    nextResponse.headers.set("Content-Type", "application/json");
+
+    return nextResponse;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
+}
